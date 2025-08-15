f4940a5c54bff93c19be551860b1960679f8be17
Pauline-Oraro <hiltrapauline@gmail.com>
2025-08-15 13:33:13 +0300

    implementing routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import CreateBooks from "./pages/CreateBooks"
+import DeleteBook from "./pages/DeleteBook"
+import EditBook from "./pages/EditBook"
+import ShowBook from "./pages/ShowBook"
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className="text-4xl font-bold">Welcome to BookNest</h1>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path="/books/details/:id" element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      </Routes>
+    </div>
   )
 }
 
